bc1b6d83127613dc7814037c839b57ebed715d46
ZaidKhan05 <zaidkhan5213@gmail.com>
2023-07-01 10:20:20 -0400

    notes

--- CSE_1322L/in class stuffs/src/notes.java
+++ CSE_1322L/in class stuffs/src/notes.java
@@ -4,9 +4,47 @@
 // arraylist: dynamic dimensional arrays, indexed at zero
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.*;
 public class notes{
+    public static void dowork(int x){
+        System.out.print(x);
+        if(x>2){
+            dowork(x-1);
+            dowork(x-2);
+        }
+        System.out.print(x);
+        
+    }
+        public static int calculate_fib_recursive(int n){
+
+        if(n == 1 || n == 2){
+            return n-1;
+        }
+        else{
+            return calculate_fib_recursive(n-1) + calculate_fib_recursive(n-2);
+        }
+    }
+    public static String reverseStringRecursive(String str){
+        if(str.length() <= 0){
+            return str.substring(0);
+        }
+        else{
+            return reverseStringRecursive(str.substring(1)) + str.substring(0);
+        }
+    }
+
     public static void main(String [] args){
+        // dowork(4);
+        String s = "hello";
+        s.substring(1);
+        System.out.println(reverseStringRecursive(s));
+        // System.out.println("enter number to be factorialed");
+        // Scanner sc = new Scanner(System.in);
+        // int x = sc.nextInt();
+        // int result = factorialRecursive(x);
+        // System.out.println(result);
+
         // ArrayList<Double> myArrayList = new ArrayList<Double>();
         // for(int i = 0; i < 10; i++){
         //     myArrayList.add(Math.random());
@@ -15,35 +53,37 @@ public class notes{
         // for (Double i : myArrayList) {
         //     System.out.println(myArrayList.indexOf(i));
         // }
-        int[] countedValues = {0,0,1,1,1,0,0,0,0,0,0,0,1,1};
-        int timesLooped = 0;
-        int count = 0;
-        for(int i = 0; i < countedValues.length; i++){
+        // int[] countedValues = {0,0,1,1,1,0,0,0,0,0,0,0,1,1};
+        // int timesLooped = 0;
+        // int count = 0;
+        // for(int i = 0; i < countedValues.length; i++){
             
-            if(timesLooped == 2){
-                System.out.println("breakloop");
-                break;
-            }
-            if(i == 13 && count < 5 && count != 0){
-                count++;
-                timesLooped++;
-                i = 0;
-            }
-            else if(countedValues[i] == 1){
-                count++;
-            }
+        //     if(timesLooped == 2){
+        //         System.out.println("breakloop");
+        //         break;
+        //     }
+        //     if(i == 13 && count < 5 && count != 0){
+        //         count++;
+        //         timesLooped++;
+        //         i = 0;
+        //     }
+        //     else if(countedValues[i] == 1){
+        //         count++;
+        //     }
             
-            else{
-                count = 0;
-            }
+        //     else{
+        //         count = 0;
+        //     }
             
-            if(count == 5){
-                System.out.println("Straight");
-            }
-        }
-        System.out.println("out of loop");
+        //     if(count == 5){
+        //         System.out.println("Straight");
+        //     }
+        // }
+        // System.out.println("out of loop");
         //}
-        // Iterator iter = myArrayList.iterator();
+        // float x = 3428593489165384l;
+        // String y = toString(x);
+        // Iterator iter = x.iterator();
         // while(iter.hasNext()){
         //     System.out.println(iter.next());
         // }
