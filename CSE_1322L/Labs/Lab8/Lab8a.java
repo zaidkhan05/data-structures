eed100de63bd46648a087901b952a15e8c080d28
ZaidKhan05 <zaidkhan5213@gmail.com>
2023-07-02 11:21:53 -0400

    lab8a done

--- CSE_1322L/Labs/Lab8/Lab8a.java
+++ CSE_1322L/Labs/Lab8/Lab8a.java
@@ -1,11 +1,219 @@
-public class Lab8a {
-   
-    public static void main(String[] Args){
-       
+
+import java.util.Scanner;
+
+public class Lab8a{
+    static int recursive_multiply(int x, int y){
+        if(y == 0){
+            return 0;
+        }
+        else{
+            return x + recursive_multiply(x, y-1);
+        }
+    }
+    static int recursive_div(int x, int y){
+        if(y == 0){
+            return -1;
+        }
+        else if(x < y){
+            return 0;
+        }
+        else if(x == y){
+            return 1;
+        }
+        else{
+            return 1 + recursive_div(x-y, y);
+        }
+    }
+    static int recursive_mod(int x, int y){
+        if(y == 0){
+            return -1;
+        }
+        else if(x < y){
+            return x;
+        }
+        // else if(x == y){
+        //     return 0;
+        // }
+        else{
+            return recursive_mod(x-y, y);
+        }
+    }
+    public static void main(String[] args){
+        //e
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+        do{
+            System.out.println("Choose from the following:");
+            System.out.println("0. Quit\n" + 
+                    "1. Multiply 2 numbers\n" + 
+                    "2. Div 2 numbers\n" + 
+                    "3. Mod 2 numbers");
+            choice = sc.nextInt();
+            switch(choice){
+                case 0:
+                    break;
+                case 1:
+                    System.out.println("Enter first number");
+                    int x = sc.nextInt();
+                    System.out.println("Enter second number");
+                    int y = sc.nextInt();
+                    System.out.println("Answer: " + recursive_multiply(x, y));
+                    break;
+                case 2:
+                    System.out.println("Enter first number");
+                    x = sc.nextInt();
+                    System.out.println("Enter second number");
+                    y = sc.nextInt();
+                    System.out.println("Answer: " + recursive_div(x, y));
+                    break;
+                case 3:
+                    System.out.println("Enter first number");
+                    x = sc.nextInt();
+                    System.out.println("Enter second number");
+                    y = sc.nextInt();
+                    System.out.println("Answer: " + recursive_mod(x, y));
+                    break;
+            }
+        }while(choice != 0);
 
     }
 }
 
 /*
+Lab 8A Tasks
+You’ll need to write 3 methods, all of which must use recursion:
+1) Write recursive_multiply. It should take in 2 integers and return an integer.
+Another way to say multiply 5*4, is 5+(5*3), or 5+5+(5*2), or 5+5+5+(5*1), or
+5+5+5+5+(5*0). You know that any number multiplied by 0 is 0.
+2) Write recursive_div. It should take in 2 integers and return an integer. Unlike regular
+division, div just returns the integer portion of division. It answers the question “how
+many times does the second number go into the first number”. So, 7 div 3 = 2, because 3
+goes in 2 times, the leftover is irrelevant to div.
+If you are asked to divide anything by 0, you should return -1, as that’s an error.
+If you are asked to divide anything by itself, you should return 1. i.e. 7 div 7 = 1
+If you are asked to divide a small number by a bigger one, the answer is 0. i.e. 2 div 7 = 0
+For all other numbers, you’ll keep subtracting the second number from the first number,
+until the first number is less than the second. You’ll count how many times this happens,
+and return that count. Remember you must use recursion in all 3 of these methods.
+3) Write recursive_mod. It should take in 2 integers and return an integer. Mod only cares
+about the remainder of division.
+If you are asked to divide anything by 0, it should return -1, as that’s an error.
+If you are asked to divide any smaller number by a larger number, you should return the
+smaller number. i.e. 2 mod 3 = 2
+For all other cases, you’ll keep subtracting the second number from the first.
+For example: 7 mod 3:
+7-3 = 4
+4-3 = 1
+Answer is 1.
+4) Finally, write the main method, which prompts the user to either multiply, div, or mod.
+Then prompt the user for 2 numbers, call the appropriate method, receive a result, and
+print it out.
+Continue to ask the user (you may use a loop here) until they choose 0 to quit.
+Lab 8A Sample Output:
+Choose from the following:
+0. Quit
+1. Multiply 2 numbers
+2. Div 2 numbers
+3. Mod 2 numbers
+1
+Enter first number
+3
+Enter second number
+5
+Answer: 15
+Choose from the following:
+0. Quit
+1. Multiply 2 numbers
+2. Div 2 numbers
+3. Mod 2 numbers
+1
+Enter first number
+5
+Enter second number
+0
+Answer: 0
+Choose from the following:
+0. Quit
+1. Multiply 2 numbers
+2. Div 2 numbers
+3. Mod 2 numbers
+2
+Enter first number
+15
+Enter second number
+5
+Answer: 3
+Choose from the following:
+0. Quit
+1. Multiply 2 numbers
+2. Div 2 numbers
+3. Mod 2 numbers
+2
+Enter first number
+15
+Enter second number
+2
+Answer: 7
+Choose from the following:
+0. Quit
+1. Multiply 2 numbers
+2. Div 2 numbers
+3. Mod 2 numbers
+2
+Enter first number
+15
+Enter second number
+0
+Answer: -1
+Choose from the following:
+0. Quit
+1. Multiply 2 numbers
+2. Div 2 numbers
+3. Mod 2 numbers
+2
+Enter first number
+2
+Enter second number
+10
+Answer: 0
+Choose from the following:
+0. Quit
+1. Multiply 2 numbers
+2. Div 2 numbers
+3. Mod 2 numbers
+3
+Enter first number
+7
+Enter second number
+2
+Answer: 1
+Choose from the following:
+0. Quit
+1. Multiply 2 numbers
+2. Div 2 numbers
+3. Mod 2 numbers
+3
+Enter first number
+10
+Enter second number
+3
+Answer: 1
+Choose from the following:
+0. Quit
+1. Multiply 2 numbers
+2. Div 2 numbers
+3. Mod 2 numbers
+3
+Enter first number
+10
+Enter second number
+0
+Answer: -1
+Choose from the following:
+0. Quit
+1. Multiply 2 numbers
+2. Div 2 numbers
+3. Mod 2 numbers
+0
 
  */
--- CSE_1322L/Labs/Lab9/Lab9.java
+++ CSE_1322L/Labs/Lab9/Lab9.java
@@ -9,5 +9,73 @@ public class Lab9 {
 }
 
 /*
-
+CSE1322L - Lab 9
+Objectives:
+In this lab you’ll allow the user to enter in 2 times in 24hr (military) format. You’ll calculate how many
+seconds are between those times.
+For example, the user may enter 12:00:00 and 14:00:00. In this case your method would return 7200
+since there are 60 seconds per minute, 60 minutes per hour, and this is 2 hours of a difference = 60*60*2
+= 7200.
+This week's lesson is on exception handling, so you’ll need to handle all the exceptions that could happen
+here. Think about what the user could do wrong. Here are a few examples:
+25:20:20 - not a valid hour
+0:0:89 - not a valid second
+14:-2:09 - not a valid minute
+12:20 - not valid because they didn’t specify seconds
+“B” - not a valid time
+“\n” - ie, they just hit enter, not valid at all
+You’ll need to handle all these, but your error should be able to produce at least 4 different errors:
+1) Hour not valid
+2) Minute not valid
+3) Second not valid
+4) Time format not valid (Covers “12:20”, “B” and “\n”)
+Tasks
+Write a method which takes in a string, and returns an integer. The string should be in the format
+HH:MM:SS for example 12:20:30
+Your method should break the string apart into 3 pieces. In java you might find string.split(“:”,3) useful,
+in C# string.Split(“:”,3)
+Convert each number to an int, and validate the sanity of each number. Hours should be 0-23, minutes
+and seconds should be 0-59. Anything outside those ranges should throw an exception of type
+InvalidTimeException with an appropriate message indicating the issue.
+Assuming the given time is valid, calculate the number of seconds since midnight of that time. The
+formula is (Hours*60*60) + (Minutes*60) + Seconds. Return that result.
+InvalidTimeException should be a class that you define that extends Exception. It should have a
+constructor which takes in a string, and calls its parent’s constructor with that string.
+Finally write a driver program which prompts the user to enter 2 time strings, and calls above method
+with the string, gets a number seconds back, and subtracts the second from the first. The main method
+should deal with all exceptions thrown. See sample run below
+Sample Output:
+Enter time 1 in 24hr format as follows (HH:MM:SS)
+12:00:00
+Enter time 2 in 24hr format as follows (HH:MM:SS)
+16:30:01
+Difference in seconds: 16201
+****Separate Run****
+Enter time 1 in 24hr format as follows (HH:MM:SS)
+26:00:00
+Hour must be below 24
+****Separate Run****
+Enter time 1 in 24hr format as follows (HH:MM:SS)
+12:20
+Enter a valid time
+****Separate Run****
+Enter time 1 in 24hr format as follows (HH:MM:SS)
+12:76:01
+Minutes must be less than 60
+****Separate Run****
+Enter time 1 in 24hr format as follows (HH:MM:SS)
+2:15:00
+Enter time 2 in 24hr format as follows (HH:MM:SS)
+4:20:60
+Seconds must be less than 60
+****Separate Run****
+Enter time 1 in 24hr format as follows (HH:MM:SS)
+Enter a valid time
+//In this last run, I just hit enter when asked for a time.
+****Separate Run****
+Enter time 1 in 24hr format as follows (HH:MM:SS)
+16:00:00
+Enter time 2 in 24hr format as follows (HH:MM:SS)
+17:00
+Enter a valid time
  */
