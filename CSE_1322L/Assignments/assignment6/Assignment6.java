caa08ad25bed68dad885e38b2aa377c3cc5daa78
zaidkhan05 <zaidkhan5213@gmail.com>
2023-07-09 23:54:26 -0400

    still need to finish assignment 6

--- CSE_1322L/Assignments/assignment6/Assignment6.java
+++ CSE_1322L/Assignments/assignment6/Assignment6.java
@@ -1,66 +1,131 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.net.*;
 
-
-
+class PersonNotFound extends Exception{
+    PersonNotFound(){}
+    PersonNotFound(String message){
+        super(message);
+    }
+}
 
 
 public class Assignment6 {
-    public static char[][] make_forward(){
-        char[][] pixel = new char[4][13];
-        pixel[0][0]=' ';
-        pixel[0][1]=' ';
-        pixel[0][2]='_';
-        pixel[0][3]='_';
-        pixel[0][4]='_';
-        pixel[0][5]='_';
-        pixel[0][6]='_';
-        pixel[0][7]='_';
-        pixel[0][8]=' ';
-        pixel[0][9]=' ';
-        pixel[0][10]=' ';
-        pixel[0][11]=' ';
-        pixel[0][12]=' ';
-        pixel[1][0]=' ';
-        pixel[1][1]='/';
-        pixel[1][2]='|';
-        pixel[1][3]='_';
-        pixel[1][4]='|';
-        pixel[1][5]='|';
-        pixel[1][6]='_';
-        pixel[1][7]='\\';
-        pixel[1][8]='\'';
-        pixel[1][9]='.';
-        pixel[1][10]='_';
-        pixel[1][11]='_';
-        pixel[1][12]=' ';
-        pixel[2][0]='(';
-        pixel[2][1]=' ';
-        pixel[2][2]=' ';
-        pixel[2][3]=' ';
-        pixel[2][4]='_';
-        pixel[2][5]=' ';
-        pixel[2][6]=' ';
-        pixel[2][7]=' ';
-        pixel[2][8]=' ';
-        pixel[2][9]='_';
-        pixel[2][10]=' ';
-        pixel[2][11]='_';
-        pixel[2][12]='\\';
-        pixel[3][0]='=';
-        pixel[3][1]='\'';
-        pixel[3][2]='-';
-        pixel[3][3]='(';
-        pixel[3][4]='_';
-        pixel[3][5]=')';
-        pixel[3][6]='-';
-        pixel[3][7]='-';
-        pixel[3][8]='(';
-        pixel[3][9]='_';
-        pixel[3][10]=')';
-        pixel[3][11]='-';
-        pixel[3][12]='\'';
-        return pixel;
+
+    static ArrayList<String> getData(){
+        ArrayList<String> data = new ArrayList<String>();
+        try{
+            Socket s = new Socket("api.coindesk.com", 80);
+            OutputStream os = s.getOutputStream();
+            PrintWriter pw = new PrintWriter(os);
+            pw.println("GET http://api.coindesk.com/v1/bpi/currentprice.json HTTP/1.0\n\n");
+            pw.flush();
+            // BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            // String line;
+            // while((line = br.readLine()) != null){
+            //     lines.add(line);
+            // }
+            // s.close();
+            Scanner sc = new Scanner(s.getInputStream());
+            while(sc.hasNextLine()){
+                data.add(sc.nextLine());
+            }
+            s.close();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return data;
     }
+
+    static float getDollarPrice(ArrayList<String> lines) {
+        boolean header=true;
+        String json="";
+        for(String line : lines) {
+            if(line.equals("")) {
+                header=false;
+                continue;
+            } 
+            if(header==false) {
+                json=line;
+                break;
+            }
+        }
+        //System.out.println("Json: "+json);
+        String[] jsonParts=json.split(":");
+        String priceLine=jsonParts[19];
+        String justPrice=priceLine.replace("},\"GBP\"","");
+        float price=Float.parseFloat(justPrice);
+        return price;
+    }
+
+    //buybitcoin
+    static void buyBitcoin(float price) {
+
+        try {
+            String dataline = "";
+            File investments = new File("initialInvestmentUSD.txt");
+            Scanner scan = new Scanner(investments);  // Note the layering
+            ArrayList<Float> btcgot = new ArrayList<Float>();
+            while (scan.hasNextLine()) {
+                dataline = scan.nextLine();
+                String[] tokens = dataline.split(":");
+                btcgot.add(Float.parseFloat(tokens[1])/price);
+                System.out.println(tokens[0]+": "+Float.parseFloat(tokens[1])/price);
+            }
+            scan.close();
+        }
+        catch (IOException ioex) {
+            System.out.println ("Error: " + ioex.getMessage());
+        }
+        
+    }
+    static void getCurrentValue(float price) {
+
+        try {
+            String dataline = "";
+            File investments = new File("clientBC.txt");
+            Scanner scan = new Scanner(investments);
+            while (scan.hasNextLine()) {
+                dataline = scan.nextLine();
+                String[] tokens = dataline.split(":");
+                System.out.println(tokens[0]+": "+Float.parseFloat(tokens[1]));
+                System.out.println(tokens[0]+": "+price);
+                System.out.println(tokens[0]+": "+Float.parseFloat(tokens[1])*price);
+            }
+            scan.close();
+        }
+        catch (IOException ioex) {
+            System.out.println ("Error: " + ioex.getMessage());
+        }
+    }
+
+
+    
     public static void main(String[] Args){
+        int choice = 0;
+        while(choice != 4){
+            System.out.println("Current Bitcoin Price: "+getDollarPrice(getData()));
+            System.out.println("1. Buy Bitcoin\n2. See everyones current value in USD\n3. See one persons gain/loss\n4. Quit");
+            Scanner sc = new Scanner(System.in);
+            choice = sc.nextInt();
+            switch(choice){
+                case 1:
+                    buyBitcoin(getDollarPrice(getData()));
+                    break;
+                case 2:
+                    getCurrentValue(getDollarPrice(getData()));
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        }
 
     }
 }
--- CSE_1322L/Labs/Lab10/Lab10.java
+++ CSE_1322L/Labs/Lab10/Lab10.java
@@ -1,66 +1,40 @@
-
-
-
-
+import java.util.Scanner;
+import java.io.*;
 
 public class Lab10 {
-    public static char[][] make_forward(){
-        char[][] pixel = new char[4][13];
-        pixel[0][0]=' ';
-        pixel[0][1]=' ';
-        pixel[0][2]='_';
-        pixel[0][3]='_';
-        pixel[0][4]='_';
-        pixel[0][5]='_';
-        pixel[0][6]='_';
-        pixel[0][7]='_';
-        pixel[0][8]=' ';
-        pixel[0][9]=' ';
-        pixel[0][10]=' ';
-        pixel[0][11]=' ';
-        pixel[0][12]=' ';
-        pixel[1][0]=' ';
-        pixel[1][1]='/';
-        pixel[1][2]='|';
-        pixel[1][3]='_';
-        pixel[1][4]='|';
-        pixel[1][5]='|';
-        pixel[1][6]='_';
-        pixel[1][7]='\\';
-        pixel[1][8]='\'';
-        pixel[1][9]='.';
-        pixel[1][10]='_';
-        pixel[1][11]='_';
-        pixel[1][12]=' ';
-        pixel[2][0]='(';
-        pixel[2][1]=' ';
-        pixel[2][2]=' ';
-        pixel[2][3]=' ';
-        pixel[2][4]='_';
-        pixel[2][5]=' ';
-        pixel[2][6]=' ';
-        pixel[2][7]=' ';
-        pixel[2][8]=' ';
-        pixel[2][9]='_';
-        pixel[2][10]=' ';
-        pixel[2][11]='_';
-        pixel[2][12]='\\';
-        pixel[3][0]='=';
-        pixel[3][1]='\'';
-        pixel[3][2]='-';
-        pixel[3][3]='(';
-        pixel[3][4]='_';
-        pixel[3][5]=')';
-        pixel[3][6]='-';
-        pixel[3][7]='-';
-        pixel[3][8]='(';
-        pixel[3][9]='_';
-        pixel[3][10]=')';
-        pixel[3][11]='-';
-        pixel[3][12]='\'';
-        return pixel;
-    }
-    public static void main(String[] Args){
 
+
+    public static void main(String[] Args){
+        Scanner sc = new Scanner(System.in);
+        //e
+        System.out.println("Enter the first file name(the full path): ");
+        String file1 = sc.nextLine();
+        System.out.println("Enter the second file name(the full path): ");
+        String file2 = sc.nextLine();
+        //open both files
+        File fileOne = new File(file1);
+        File fileTwo = new File(file2);
+        //compare the lines
+        try {
+            Scanner input1 = new Scanner(fileOne);
+            Scanner input2 = new Scanner(fileTwo);
+            int lineNum = 1;
+            while (input1.hasNextLine() && input2.hasNextLine()) {
+                String line1 = input1.nextLine();
+                String line2 = input2.nextLine();
+                if (!line1.equals(line2)) {
+                    System.out.println("Line " + lineNum);
+                    System.out.println("< " + line1);
+                    System.out.println("> " + line2);
+                }
+                lineNum++;
+            }
+            if (input1.hasNextLine() || input2.hasNextLine()) {
+                System.out.println("Files have different number of lines");
+            }
+        } 
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
