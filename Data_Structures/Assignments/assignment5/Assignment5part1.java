d7b93efa0ab6554cd152776220b72b0e78dbebb3
ZaidKhan05 <zaidkhan5213@gmail.com>
2024-02-20 17:01:10 -0500

    why it not sorting it properly pain

--- Data_Structures/Assignments/assignment5/Assignment5part1.java
+++ Data_Structures/Assignments/assignment5/Assignment5part1.java
@@ -8,30 +8,78 @@
 import java.util.LinkedList;
 import java.util.Scanner;
 
+class Queue extends LinkedList<String>{
+    public void add(int priority, String chore){
+        if(this.isEmpty()){
+            this.add(chore + " - " + priority);
+        }
+        else{
+            //sort the list based on priority
+            for(int i = 0; i < this.size(); i++){
+                for(int j = 0; j < this.size(); j++){
+
+                String[] temp = this.get(i).split(" - ");
+                
+                if(priority < Integer.parseInt(temp[1])){
+                    String temp2 = this.get(i);
+                    this.remove(i);
+                    this.add(chore + " - " + priority);
+                    this.add(temp2);
+                    return;
+                }
+                
+            }
+            }
+            this.add(chore + " - " + priority);
+        }
+    }
+    public void dequeue(){
+        if(!this.isEmpty()){
+            this.removeFirst();
+            System.out.println("you have completed" + this.getFirst() + "!, now you have to do " + this.getFirst());    
+        }
+        else{
+            System.out.println("The list is empty");
+        }
+    }
+
+    
+}
+
 
 public class Assignment5part1 {
     public static void main(String[] args) {
         //create a new linked list
-        LinkedList<String> list = new LinkedList<String>();
+        Queue chores = new Queue();
         System.out.println("Lets make a list for the chores we need to do!");
         Scanner sc = new Scanner(System.in);
-        while(true){
+        boolean addMore = true;
+        while(addMore){
             // list.add("");
             System.out.println("Enter a chore: ");
             String chore = sc.nextLine();
             System.out.println("Enter the priority of the chore: ");
             int priority = sc.nextInt();
             sc.nextLine();
-            list.add(priority, chore);
+            chores.add(priority, chore);
+
             System.out.println("Do you want to add another chore? (y/n)");
-            if(sc.next().equalsIgnoreCase("n")){
-                break;
+            String temp = sc.nextLine();
+            if(temp.equalsIgnoreCase("n")){
+                addMore = false;
+            }
+            else if(temp.equalsIgnoreCase("y")){
+                addMore = true;
+            }
+            else{
+                System.out.println("Invalid input, assuming you want to add another chore");
+                addMore = true;
             }
         }
         System.out.println("Here is the list of chores and their priorities: ");
         sc.close();
 
-        System.out.println(list);
+        System.out.println(chores);
     }
     
 }
