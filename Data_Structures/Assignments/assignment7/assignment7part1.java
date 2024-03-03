a7fc9a0aba53c450d555f7f2c056cecd2c03a9cd
ZaidKhan05 <zaidkhan5213@gmail.com>
2024-03-03 12:26:38 -0500

    7p1 done

--- Data_Structures/Assignments/assignment7/assignment7part1.java
+++ Data_Structures/Assignments/assignment7/assignment7part1.java
@@ -1,5 +1,6 @@
 
 import java.util.*;
+import java.util.LinkedList;
 import java.io.*;
 
 public class assignment7part1 {
@@ -27,12 +28,40 @@ public class assignment7part1 {
         //return the merged lists
         return merge(left, right);
     }
+    //merge method
+    public static LinkedList<Integer> merge(LinkedList<Integer> left, LinkedList<Integer> right){
+        //create a new linked list
+        LinkedList<Integer> merged = new LinkedList<Integer>();
+        //while both lists are not empty
+        while(!left.isEmpty() && !right.isEmpty()){
+            //if the first element in the left list is less than the first element in the right list
+            if(left.getFirst() < right.getFirst()){
+                //add the first element in the left list to the merged list
+                merged.add(left.removeFirst());
+            }
+            //if the first element in the right list is less than the first element in the left list
+            else{
+                //add the first element in the right list to the merged list
+                merged.add(right.removeFirst());
+            }
+        }
+        //if the left list is not empty, add the remaining elements to the merged list
+        while(!left.isEmpty()){
+            merged.add(left.removeFirst());
+        }
+        //if the right list is not empty, add the remaining elements to the merged list
+        while(!right.isEmpty()){
+            merged.add(right.removeFirst());
+        }
+        //return the merged list
+        return merged;
+    }
     public static void main(String[] args) {
         //create a new linked list
         LinkedList<Integer> list = new LinkedList<Integer>();
         //add the integers from the file to the linked list
         try{
-            File file = new File("mergetest.txt");
+            File file = new File("C:\\Users\\agent\\data-structures-1\\Data_Structures\\Assignments\\assignment7\\mergetest.txt");
             Scanner sc = new Scanner(file);
             while(sc.hasNextInt()){
                 list.add(sc.nextInt());
--- Data_Structures/Assignments/assignment7/assignment7part2.java
+++ Data_Structures/Assignments/assignment7/assignment7part2.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.*;
+
 public class assignment7part2 {
-    
+    public static void main(String[] args) {
+        //e
+        Scanner input = new Scanner(System.in);
+        //create a new heap
+        Heap<Integer> heap = new Heap<Integer>();
+        //prompt the user to enter 5 numbers
+        System.out.println("Enter 5 numbers: ");
+
+    }
 }
+/*
+Part 2 - Objective of Part 2 is to reinforce understanding of Heap data structures.
+
+You may use the Java Libraries for solving this problem. We recommend using java.util.* The star “ * ” acts as a wildcard and allows you to use the entire java.util library. For more information on this library review the tutorial https://docs.oracle.com/javase/tutorial/collections/intro/index.html
+
+The heap presented in our textbook is also known as a max-heap, in which each node is greater than or equal to any of its children. A min-heap is a heap in which each node is less than or equal to any of its children. Min-heaps are often used to implement priority queues. Revise the Heap class in Listing 23.9 (which starts on p.878) to implement a min-heap.
+
+Use the following logic:
+
+1.	Write a main method that will accept 5 numbers, and put them in a min-heap
+
+2.	Remove them from the min heap, printing one at a time as they are removed, to show that the list is sorted
+
+Hint: the textbook example is for a MAX heap; you must alter that example to reflect a MIN heap – think about it !
+
+No files or data are provided for this part of the assignment.
+
+ */
