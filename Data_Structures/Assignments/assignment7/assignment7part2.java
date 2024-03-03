79352aeb1f23dd8a030bfcb97f3eed136d0067af
ZaidKhan05 <zaidkhan5213@gmail.com>
2024-03-03 12:43:54 -0500

    heap thing

--- Data_Structures/Assignments/assignment7/assignment7part2.java
+++ Data_Structures/Assignments/assignment7/assignment7part2.java
@@ -1,15 +1,89 @@
 import java.util.Scanner;
 import java.util.LinkedList;
-import java.util.*;
+// import java.util.*;
+
+class MinHeap<E extends Comparable<E>> {
+    private LinkedList<E> list = new LinkedList<E>();
+
+    public MinHeap() {
+    }
+
+    public MinHeap(E[] objects) {
+        for (int i = 0; i < objects.length; i++)
+            add(objects[i]);
+    }
+
+    public void add(E newObject) {
+        list.add(newObject);
+        int currentIndex = list.size() - 1;
+
+        while (currentIndex > 0) {
+            int parentIndex = (currentIndex - 1) / 2;
+            if (list.get(currentIndex).compareTo(list.get(parentIndex)) < 0) {
+                E temp = list.get(currentIndex);
+                list.set(currentIndex, list.get(parentIndex));
+                list.set(parentIndex, temp);
+            } else
+                break;
+            currentIndex = parentIndex;
+        }
+    }
+
+    public E remove() {
+        if (list.size() == 0)
+            return null;
+
+        E removedObject = list.get(0);
+        list.set(0, list.get(list.size() - 1));
+        list.remove(list.size() - 1);
+
+        int currentIndex = 0;
+        while (currentIndex < list.size()) {
+            int leftChildIndex = 2 * currentIndex + 1;
+            int rightChildIndex = 2 * currentIndex + 2;
+
+            if (leftChildIndex >= list.size())
+                break;
+            int maxIndex = leftChildIndex;
+            if (rightChildIndex < list.size()) {
+                if (list.get(maxIndex).compareTo(list.get(rightChildIndex)) > 0) {
+                    maxIndex = rightChildIndex;
+                }
+            }
+            if (list.get(currentIndex).compareTo(list.get(maxIndex)) > 0) {
+                E temp = list.get(maxIndex);
+                list.set(maxIndex, list.get(currentIndex));
+                list.set(currentIndex, temp);
+                currentIndex = maxIndex;
+            } else
+                break;
+        }
+        return removedObject;
+    }
+
+    public int getSize() {
+        return list.size();
+    }
+}
 
 public class assignment7part2 {
     public static void main(String[] args) {
         //e
         Scanner input = new Scanner(System.in);
         //create a new heap
-        Heap<Integer> heap = new Heap<Integer>();
+        MinHeap<Integer> heap = new MinHeap<Integer>();
         //prompt the user to enter 5 numbers
         System.out.println("Enter 5 numbers: ");
+        //add the numbers to the heap
+        for(int i = 0; i < 5; i++){
+            heap.add(input.nextInt());
+        }
+        //remove the numbers from the heap and print them
+        for(int i = 0; i < 5; i++){
+            System.out.println(heap.remove());
+            
+        }
+
 
     }
 }
